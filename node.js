f24f60816878d61072f3d7abc59980bb862d76c2
sanskrati-1234 <sanskratiagrawal306@gmail.com>
2022-04-15 22:00:52 +0530

    react-notes

--- node.js
+++ node.js
@@ -3,20 +3,16 @@ const http = require("http");
 const server = http.createServer((req, res) => {
   if (req.method === "POST") {
     res.write("POST");
-    res.end();
   } else if (req.method === "GET") {
     res.write("GET");
-    res.end();
   } else if (req.method === "PUT") {
     res.write("PUT");
-    res.end();
   } else if (req.method === "PATCH") {
     res.write("PATCH");
-    res.end();
   } else if (req.method === "DELETE") {
     res.write("DELETE");
-    res.end();
   }
+  res.end();
 });
 
 server.listen(3000);
