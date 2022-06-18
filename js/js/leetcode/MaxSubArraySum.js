3fec49d83c87950564b3c427665b5ecda6e10ec2
sanskrati-1234 <sanskratiagrawal306@gmail.com>
2022-06-18 15:23:30 +0530

    ps

---
+++ js/js/leetcode/MaxSubArraySum.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSubArray = function (nums) {
+  if (nums.length === 0) {
+    return 0;
+  }
+  let currentsum = nums[0];
+  let sum = nums[0];
+  for (let i = 1; i < nums.length; i++) {
+    currentsum = Math.max(currentsum + nums[i], nums[i]);
+    sum = Math.max(currentsum, sum);
+  }
+  return sum;
+};
