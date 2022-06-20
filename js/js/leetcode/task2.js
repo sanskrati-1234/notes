c1f428ebe3e87b5eacb282495e1f21991d029d76
sanskrati-1234 <sanskratiagrawal306@gmail.com>
2022-06-20 21:56:28 +0530

    ps

---
+++ js/js/leetcode/task2.js
@@ -0,0 +1,14 @@
+function XO(str) {
+  let xcount = 0,
+    ocount = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "x" || str[i] === "X") {
+      xcount++;
+    } else if (str[i] === "o" || str[i] === "0") {
+      ocount++;
+    }
+  }
+  return xcount === ocount;
+}
+
+console.log(XO("xxoo"));
