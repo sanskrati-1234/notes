9557f8690c79d8919b7811e4c83dc8a2dcc9aecd
sanskrati-1234 <sanskratiagrawal306@gmail.com>
2022-06-26 21:36:35 +0530

    DSA

--- js/DSA-26-JUNE/LargestBand.js
+++ js/DSA-26-JUNE/LargestBand.js
@@ -1,5 +1,14 @@
 let arr = [1, 9, 3, 0, 18, 5, 2, 4, 10, 7, 12, 6];
 
-let obj={}
+let obj = {};
 
-for()
+for (let i = 0; i < arr.length; i++) {
+  let current = arr[i];
+  if (!arr.includes(arr[i] - 1)) {
+    obj[arr[i]] = 1;
+    while (arr.includes(++current)) {
+      obj[arr[i]] = obj[arr[i]] + 1;
+    }
+  }
+}
+console.log(Math.max(...Object.values(obj)));
